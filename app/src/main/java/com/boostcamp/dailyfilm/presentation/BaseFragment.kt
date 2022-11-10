0b3c168e0afe80cdd37e60a08b416051ef4ee75e
eoeo0326 <eoeo0326@gmail.com>
2022-11-10 10:37:06 +0900

    issue #3 chore: Base 적용, Hilt 적용, Activity Logging

---
+++ app/src/main/java/com/boostcamp/dailyfilm/presentation/BaseFragment.kt
@@ -0,0 +1,41 @@
+package com.boostcamp.dailyfilm.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<B : ViewDataBinding>(
+    @LayoutRes val layoutId: Int
+) : Fragment() {
+
+    private var _binding: B? = null
+    protected val binding get() = _binding ?: error("Binding is null")
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = DataBindingUtil.inflate(inflater, layoutId, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        init()
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+
+    abstract fun init()
+}
