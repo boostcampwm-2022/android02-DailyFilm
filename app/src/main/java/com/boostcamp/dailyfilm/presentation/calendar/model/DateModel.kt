f0d2c24519790943ab6db073037c074054d1164a
junhyeongleeee <djskal3745@gmail.com>
2022-11-15 22:30:17 +0900

    feat: presentation DateModel 추가
    Co-authored-by: junhyeongleeee <djskal3745@gmail.com>
    Co-authored-by: eoeo0326 <eoeo0326@gmail.com>

---
+++ app/src/main/java/com/boostcamp/dailyfilm/presentation/calendar/model/DateModel.kt
@@ -0,0 +1,10 @@
+package com.boostcamp.dailyfilm.presentation.calendar.model
+
+import java.time.Month
+
+data class DateModel(
+    val year: String,
+    val month: String,
+    val day: String,
+    val imgUrl: String,
+)
