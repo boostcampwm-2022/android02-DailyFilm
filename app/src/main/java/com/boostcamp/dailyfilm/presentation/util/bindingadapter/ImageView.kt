e0a4f5625b0d5039b6ba3aced6fe2e312996415f
eoeo0326 <eoeo0326@gmail.com>
2022-11-21 18:26:09 +0900

    issue #17 feat: Glide 메모리 이슈 해결, 달력 아이템 높이 동적으로 변경
    Co-authored-by: junhyeongleeee <djskal3745@gmail.com>
    Co-authored-by: eoeo0326 <eoeo0326@gmail.com>

--- app/src/main/java/com/boostcamp/dailyfilm/presentation/util/bindingadapter/ImageView.kt
+++ app/src/main/java/com/boostcamp/dailyfilm/presentation/util/bindingadapter/ImageView.kt
@@ -3,22 +3,20 @@ package com.boostcamp.dailyfilm.presentation.util.bindingadapter
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.boostcamp.dailyfilm.R
-import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestManager
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.transition.DrawableCrossFadeFactory
 
+private val factory = DrawableCrossFadeFactory.Builder().setCrossFadeEnabled(true).build()
 
-@BindingAdapter(value = ["loadUrl", "cornerRadius"], requireAll = false)
-fun ImageView.loadImage(imageUrl: String?, cornerRadius: Int? = null) {
+@BindingAdapter(value = ["glide", "loadUrl", "cornerRadius"], requireAll = false)
+fun ImageView.loadImage(glide: RequestManager, imageUrl: String?, cornerRadius: Int? = null) {
     imageUrl ?: return
-    val factory = DrawableCrossFadeFactory.Builder().setCrossFadeEnabled(true).build()
-
-    Glide.with(context)
-        .load(imageUrl)
+    glide.load(imageUrl)
         .transition(DrawableTransitionOptions.withCrossFade(factory))
-        .placeholder(R.color.white)
+        .placeholder(R.color.gray)
         .let { builder ->
             if (cornerRadius != null) {
                 builder.transform(CenterCrop(), RoundedCorners(cornerRadius))
@@ -27,6 +25,4 @@ fun ImageView.loadImage(imageUrl: String?, cornerRadius: Int? = null) {
             }
         }
         .into(this)
-
-
 }
