94d20a6ae1bb7ed38dc23375851b1e4621bc3dcf
eoeo0326 <eoeo0326@gmail.com>
2022-12-01 13:17:22 +0900

    issue #44 feat: film 업로드시 Room에 추가, Hilt Module 수정
    Co-authored-by: junhyeongleeee <djskal3745@gmail.com>
    Co-authored-by: eoeo0326 <eoeo0326@gmail.com>

--- app/src/main/java/com/boostcamp/dailyfilm/data/uploadfilm/UploadFilmRepository.kt
+++ app/src/main/java/com/boostcamp/dailyfilm/data/uploadfilm/UploadFilmRepository.kt
@@ -1,7 +1,7 @@
 package com.boostcamp.dailyfilm.data.uploadfilm
 
 import android.net.Uri
-import android.util.Log
+import com.boostcamp.dailyfilm.data.calendar.CalendarDataSource
 import com.boostcamp.dailyfilm.data.model.DailyFilmItem
 import com.boostcamp.dailyfilm.data.model.Result
 import com.boostcamp.dailyfilm.presentation.uploadfilm.model.DateAndVideoModel
@@ -13,15 +13,20 @@ interface UploadFilmRepository {
 
     fun uploadFilmInfo(userId: String, uploadDate:String, filmInfo: DailyFilmItem): Flow<Result<Unit>>
 
+    suspend fun insertFilmEntity(filmInfo: DailyFilmItem)
 }
 
 class UploadFilmRepositoryImpl @Inject constructor(
-    private val uploadFilmDataSource: UploadFilmDataSource
+    private val uploadFilmDataSource: UploadFilmDataSource,
+    private val calendarDataSource: CalendarDataSource
 ) : UploadFilmRepository {
 
     override fun uploadVideo(videoUri: Uri) = uploadFilmDataSource.uploadVideo(videoUri)
 
-    override fun uploadFilmInfo(userId: String, uploadDate:String, filmInfo: DailyFilmItem) =
+    override fun uploadFilmInfo(userId: String, uploadDate: String, filmInfo: DailyFilmItem) =
         uploadFilmDataSource.uploadFilmInfo(userId, uploadDate, filmInfo)
 
+    override suspend fun insertFilmEntity(filmInfo: DailyFilmItem) {
+        calendarDataSource.insertFilm(filmInfo.mapToFilmEntity())
+    }
 }
