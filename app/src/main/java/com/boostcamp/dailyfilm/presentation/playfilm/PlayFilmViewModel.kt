6d8d1c3262cd17c5be8d012a46a9501a6b452c6c
DongHyeon Park <paramore@kakao.com>
2022-12-08 20:35:29 +0900

    issue #55 feat: 내용 작성 화면 텍스트 숨기기 버튼 추가 및 둥근 백그라운드 적용

--- app/src/main/java/com/boostcamp/dailyfilm/presentation/playfilm/PlayFilmViewModel.kt
+++ app/src/main/java/com/boostcamp/dailyfilm/presentation/playfilm/PlayFilmViewModel.kt
@@ -1,5 +1,7 @@
 package com.boostcamp.dailyfilm.presentation.playfilm
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.boostcamp.dailyfilm.presentation.calendar.model.DateModel
@@ -10,4 +12,12 @@ class PlayFilmViewModel @Inject constructor(
 ) : ViewModel() {
     val dateModel = savedStateHandle.get<DateModel>(PlayFilmFragment.KEY_DATE_MODEL)
         ?: throw IllegalStateException("PlayFilmViewModel - DateModel is null")
+
+    private val _isContentShowed = MutableLiveData(true)
+    val isContentShowed: LiveData<Boolean> get() = _isContentShowed
+
+    fun changeShowState(){
+        _isContentShowed.value = _isContentShowed.value?.not()
+    }
+
 }
