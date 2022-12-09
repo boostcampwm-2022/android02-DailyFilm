a4bf349c626af245218cbc8da10342dd3d5b809e
DongHyeon Park <paramore@kakao.com>
2022-12-09 17:30:21 +0900

    issue #69 feat: 전체 재생 화면 음소거 기능 추가

--- app/src/main/java/com/boostcamp/dailyfilm/presentation/totalfilm/TotalfilmBindingAdapter.kt
+++ app/src/main/java/com/boostcamp/dailyfilm/presentation/totalfilm/TotalfilmBindingAdapter.kt
@@ -1,6 +1,8 @@
 package com.boostcamp.dailyfilm.presentation.totalfilm
 
+import android.animation.ValueAnimator
 import androidx.databinding.BindingAdapter
+import com.airbnb.lottie.LottieAnimationView
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.Player
@@ -10,7 +12,7 @@ import com.google.android.exoplayer2.ui.StyledPlayerView
 fun StyledPlayerView.playTotalVideo(viewModel: TotalFilmViewModel) {
     if (player == null) {
         player = ExoPlayer.Builder(context).build().apply {
-            volume = 0f
+            volume = 0.5f
             setPlaybackSpeed(2.0f)
         }
     }
@@ -35,4 +37,40 @@ fun StyledPlayerView.playTotalVideo(viewModel: TotalFilmViewModel) {
             }
         })
     }
+}
+
+@BindingAdapter("changeVolume")
+fun StyledPlayerView.changeVolume(isMuted: Boolean) {
+    player?.volume =
+        when (isMuted) {
+            true -> {
+                0.0f
+            }
+            false -> {
+                0.5f
+            }
+        }
+}
+
+@BindingAdapter("syncMuteIcon")
+fun LottieAnimationView.syncMuteIcon(isMuted: Boolean) {
+    val animator: ValueAnimator =
+        when (isMuted) {
+            true -> {
+                ValueAnimator.ofFloat(0.0f, 0.5f).apply {
+                    duration = 500
+                }
+            }
+            false -> {
+                ValueAnimator.ofFloat(0.5f, 1.0f).apply {
+                    duration = 500
+                }
+            }
+        }.apply {
+            addUpdateListener {
+                progress = it.animatedValue as Float
+            }
+        }.also {
+            it.start()
+        }
 }
