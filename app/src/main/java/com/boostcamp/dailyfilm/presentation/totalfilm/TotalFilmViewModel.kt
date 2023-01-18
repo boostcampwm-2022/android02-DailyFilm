b30a0de279e034fb17ed037fbc90ee1f9872a0e2
eoeo0326 <eoeo0326@gmail.com>
2023-01-18 21:31:29 +0900

    issue #99 feat: 한 달 영상 전체 재생 Local URI 로 하도록 변경

--- app/src/main/java/com/boostcamp/dailyfilm/presentation/totalfilm/TotalFilmViewModel.kt
+++ app/src/main/java/com/boostcamp/dailyfilm/presentation/totalfilm/TotalFilmViewModel.kt
@@ -1,19 +1,19 @@
 package com.boostcamp.dailyfilm.presentation.totalfilm
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.ViewModel
+import android.net.Uri
+import androidx.lifecycle.*
+import com.boostcamp.dailyfilm.data.model.Result
+import com.boostcamp.dailyfilm.data.playfilm.PlayFilmRepository
 import com.boostcamp.dailyfilm.presentation.calendar.CalendarActivity
 import com.boostcamp.dailyfilm.presentation.calendar.model.DateModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class TotalFilmViewModel @Inject constructor(
+    private val playFilmRepository: PlayFilmRepository,
     savedStateHandle: SavedStateHandle
 ) : ViewModel() {
 
@@ -21,6 +21,9 @@ class TotalFilmViewModel @Inject constructor(
 
     private val _currentDateItem = MutableStateFlow(filmArray?.get(0))
     val currentDateItem: StateFlow<DateModel?> = _currentDateItem.asStateFlow()
+
+    private val _downloadedVideoUri = MutableSharedFlow<Uri?>()
+    val downloadedVideoUri: SharedFlow<Uri?> = _downloadedVideoUri.asSharedFlow()
 
     private val _isContentShowed = MutableLiveData(true)
     val isContentShowed: LiveData<Boolean> get() = _isContentShowed
@@ -35,6 +38,10 @@ class TotalFilmViewModel @Inject constructor(
         _currentDateItem.value = dateModel
     }
 
+    init {
+        loadVideos()
+    }
+
     fun changeShowState() {
         _isContentShowed.value = _isContentShowed.value?.not()
     }
@@ -45,5 +52,47 @@ class TotalFilmViewModel @Inject constructor(
 
     fun changeEndState() {
         _isEnded.value = _isEnded.value.not()
+    }
+
+    private fun loadVideos() {
+        viewModelScope.launch {
+            filmArray?.forEach { dateModel ->
+                val updateDate = dateModel.getDate()
+                launch {
+                    playFilmRepository.checkVideo(updateDate).collectLatest { localResult ->
+                        when (localResult) {
+                            is Result.Uninitialized -> return@collectLatest
+                            is Result.Success -> {
+                                if (localResult.data != null) {
+                                    _downloadedVideoUri.emit(localResult.data)
+                                } else {
+                                    playFilmRepository.downloadVideo(updateDate)
+                                        .collectLatest { remoteResult ->
+                                            when (remoteResult) {
+                                                is Result.Uninitialized -> {}
+                                                is Result.Success -> {
+                                                    val localUri = remoteResult.data
+                                                    _downloadedVideoUri.emit(localUri)
+                                                    playFilmRepository.insertVideo(updateDate, localUri.toString())
+                                                        .collectLatest { insertResult ->
+                                                            when (insertResult) {
+                                                                is Result.Uninitialized -> {}
+                                                                is Result.Success -> {}
+                                                                is Result.Error -> {}
+                                                            }
+                                                        }
+                                                }
+                                                is Result.Error -> {}
+                                            }
+                                        }
+                                }
+                            }
+                            is Result.Error -> {}
+                        }
+                    }
+                }
+            }
+            _downloadedVideoUri.emit(Uri.EMPTY)
+        }
     }
 }
