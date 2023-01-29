cd08eb9bbf63f1101beca683a12d657e7528eb6e
DongHyeon Park <paramore@kakao.com>
2023-01-29 23:26:34 +0900

    issue #101 feat: 압축 완료시 배경에 영상 재생

--- app/src/main/java/com/boostcamp/dailyfilm/presentation/uploadfilm/UploadFilmBindingAdapter.kt
+++ app/src/main/java/com/boostcamp/dailyfilm/presentation/uploadfilm/UploadFilmBindingAdapter.kt
@@ -1,12 +1,22 @@
 package com.boostcamp.dailyfilm.presentation.uploadfilm
 
 import android.animation.ValueAnimator
+import android.net.Uri
+import android.util.Log
 import android.widget.EditText
 import androidx.databinding.BindingAdapter
 import com.airbnb.lottie.LottieAnimationView
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.ui.StyledPlayerView
 
 @BindingAdapter(value = ["updateAnimation", "inputText", "showKeyboard"], requireAll = false)
-fun LottieAnimationView.updateAnimation(isWriting: Boolean, text: EditText, activity: UploadFilmActivity) {
+fun LottieAnimationView.updateAnimation(
+    isWriting: Boolean,
+    text: EditText,
+    activity: UploadFilmActivity
+) {
     lateinit var animator: ValueAnimator
 
     when (isWriting) {
@@ -30,4 +40,24 @@ fun LottieAnimationView.updateAnimation(isWriting: Boolean, text: EditText, acti
         progress = it.animatedValue as Float
     }
     animator.start()
+}
+
+@BindingAdapter(value = ["willBePlayed", "compressProgress"], requireAll = false)
+fun StyledPlayerView.playVideoWhenReady(uri: Uri?, progress: Int) {
+    if (player == null) {
+        player = ExoPlayer.Builder(context).build().apply {
+            volume = 0.5f
+            playWhenReady = true
+            repeatMode = Player.REPEAT_MODE_ONE
+        }
+    }
+
+    if (progress == 100){
+        uri?.let { videoUri ->
+            val mediaItem = MediaItem.fromUri(videoUri)
+            player?.setMediaItem(mediaItem)
+            player?.prepare()
+            player?.play()
+        }
+    }
 }
