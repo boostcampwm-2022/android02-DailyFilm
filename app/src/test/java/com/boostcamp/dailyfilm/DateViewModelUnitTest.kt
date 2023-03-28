75753b59245d80c915c9ec1cbcc6b569a0dbc42d
junhyeongleeee <djskal3745@gmail.com>
2023-03-28 16:50:26 +0900

    issue #142 test: viewModel 초기화
    Co-authored-by: junhyeongleeee <djskal3745@gmail.com>

--- app/src/test/java/com/boostcamp/dailyfilm/DateViewModelUnitTest.kt
+++ app/src/test/java/com/boostcamp/dailyfilm/DateViewModelUnitTest.kt
@@ -5,10 +5,8 @@ import com.boostcamp.dailyfilm.data.calendar.CalendarRepository
 import com.boostcamp.dailyfilm.data.sync.SyncRepository
 import com.boostcamp.dailyfilm.presentation.calendar.DateFragment.Companion.KEY_CALENDAR
 import com.boostcamp.dailyfilm.presentation.calendar.DateViewModel
-import com.boostcamp.dailyfilm.presentation.util.getStartCalendar
-import junit.framework.Assert.assertEquals
+import com.boostcamp.dailyfilm.presentation.util.*
 import org.junit.Before
-import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito
 import org.mockito.junit.MockitoJUnitRunner
@@ -33,44 +31,5 @@ class DateViewModelUnitTest {
                 )
             }
         dateViewModel = DateViewModel(calendarRepository, syncRepository, "", savedStateHandle)
-    }
-
-    @Test
-    fun example1() {
-        val calendar = Calendar.getInstance().apply {
-            set(Calendar.YEAR, 2023)
-            set(Calendar.MONTH, 1)
-            set(Calendar.DAY_OF_WEEK, 15)
-        }
-        val maxDay = 28
-        val dayOfWeek = 4
-
-        val startCalendar = getStartCalendar(calendar, maxDay, dayOfWeek)
-        val formatCalendar = dateFormat.format(startCalendar.time)
-        assertEquals(formatCalendar, "20230226")
-    }
-
-    @Test
-    fun example2() {
-        val calendar = Calendar.getInstance().apply {
-            set(Calendar.YEAR, 2023)
-            set(Calendar.MONTH, 10)
-            set(Calendar.DAY_OF_WEEK, 15)
-        }
-        val maxDay = 30
-        val dayOfWeek = 6
-
-        val startCalendar = getStartCalendar(calendar, maxDay, dayOfWeek)
-        val formatCalendar = dateFormat.format(startCalendar.time)
-        assertEquals(formatCalendar, "20231126")
-    }
-
-    @Test
-    fun month() {
-        val calendar = Calendar.getInstance(Locale.getDefault()).apply {
-            set(Calendar.MONTH, 3)
-        }
-        val formatCalendar = dateFormat.format(calendar.time)
-        assertEquals(formatCalendar, "20230322")
     }
 }
