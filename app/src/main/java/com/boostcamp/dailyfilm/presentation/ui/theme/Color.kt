9f93985d3518b830d42ecf5cc94a58ad53746b0b
junhyeongleeee <djskal3745@gmail.com>
2023-05-23 17:18:39 +0900

    issue #152 chore: 테마 색깔 수정
    Co-authored-by: junhyeongleeee <djskal3745@gmail.com>

--- app/src/main/java/com/boostcamp/dailyfilm/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/boostcamp/dailyfilm/presentation/ui/theme/Color.kt
@@ -5,11 +5,15 @@ import androidx.compose.ui.graphics.Color
 val primary = Color(0xFF202022)
 val onPrimary = Color(0xFFFFFFFF)
 val surface = Color(0xFFFFFFFF)
-val onSurface = Color(0xFF000000)
+val onSurface = Color(0xFF202022)
 
 
 // nights
 val darkPrimary = Color(0xFFFFFFFF)
 val darkOnPrimary = Color(0xFFFFFFFF)
-val darkSurface = Color(0xFF121212)
+val darkSurface = Color(0xFF202022)
 val darkOnSurface = Color(0xFFFFFFFF)
+
+val white = Color(0xFFFFFFFF)
+val lightBlack = Color(0xFF202022)
+val black = Color(0xFF000000)
--- app/src/main/java/com/boostcamp/dailyfilm/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/boostcamp/dailyfilm/presentation/ui/theme/Theme.kt
@@ -5,29 +5,37 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import com.example.composeapplication.ui.theme.Shapes
 import com.example.composeapplication.ui.theme.Typography
+import com.example.composeapplication.ui.theme.black
 import com.example.composeapplication.ui.theme.darkOnPrimary
 import com.example.composeapplication.ui.theme.darkOnSurface
 import com.example.composeapplication.ui.theme.darkPrimary
 import com.example.composeapplication.ui.theme.darkSurface
+import com.example.composeapplication.ui.theme.lightBlack
 import com.example.composeapplication.ui.theme.onPrimary
 import com.example.composeapplication.ui.theme.onSurface
 import com.example.composeapplication.ui.theme.primary
 import com.example.composeapplication.ui.theme.surface
+import com.example.composeapplication.ui.theme.white
 
 private val DarkColorPalette = darkColors(
-    surface = darkSurface,
-    onSurface = darkOnSurface,
-    primary = darkPrimary,
-    onPrimary = darkOnPrimary
+    surface = lightBlack,
+    onSurface = white,
+    primary = black,
+    onPrimary = black,
+    background = lightBlack,
+    onBackground = white
 )
 
 private val LightColorPalette = lightColors(
-    surface = surface,
-    onSurface = onSurface,
-    primary = primary,
-    onPrimary = onPrimary
+    surface = white,
+    onSurface = black,
+    primary = Color(0xFFF0F0F0),
+    onPrimary = white,
+    onBackground = black,
+    background = white
 )
 
 @Composable
